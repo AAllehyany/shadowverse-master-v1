69698efe9dae345c1012ebb2d662b3c43c2786ac
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-22 02:19:35 +0300

    basic layout view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary": "#020202",
+				"secondary": "#71bf67",
+				"accent": "#f8ac11"
+			}
+		},
 	},
 	plugins: [],
 }
