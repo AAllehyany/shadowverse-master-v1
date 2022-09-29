4b6ed6fe12de850fde5637005356ffa3f712220a
Ahmad Allehyany <aallehyany@gmail.com>
2022-09-29 04:13:06 +0300

    added twitter provider

--- firebaseSettings.js
+++ firebaseSettings.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
-import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getAuth, TwitterAuthProvider} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +21,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const twitterProvider = new TwitterAuthProvider();
