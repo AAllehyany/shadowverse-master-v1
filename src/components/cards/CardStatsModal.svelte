5a3eaf3c565f65fc68f573633a3d97852798ff6e
Ahmad Allehyany <aallehyany@gmail.com>
2022-10-05 03:17:57 +0300

    new feature to display card stats

---
+++ src/components/cards/CardStatsModal.svelte
@@ -0,0 +1,34 @@
+<script>
+  import modal from '../../modalStore'
+
+  const getImage = id => (`https://ik.imagekit.io/svmaster/tr:w-200/cards/${id}.png`)
+
+  function hideModal() {
+    modal.set({open: false, card: null})
+  }
+</script>
+
+{#if $modal && $modal.open}
+<div class="fixed w-full h-full bg-black/90 z-50" on:click|stopPropagation={hideModal}>
+  <div id="modal">
+    <img src={getImage($modal.card.id)} class="rounded-lg w-48"/>
+    <h2 class="text-md font-heading text-primary mt-3 font-bold">{$modal.card.name}</h2>
+
+    <p class="text-sm font-heading text-primary mt-5 font-bold">Usage Breakdown:</p>
+    <div class="flex items-center mt-2">
+      <span class="bg-green-400 px-2 py-1 text-sm rounded-md text-white mr-3">average: {$modal.card.average}</span>
+      <span class="bg-yellow-400 px-2 py-1 text-sm rounded-md text-white">total: {$modal.card.total}</span>
+    </div>
+    <button on:click={hideModal} class="absolute bottom-1 bg-red-800 px-4 py-2 text-white font-bold rounded-md">Close</button>
+  </div>
+</div>
+{/if}
+
+<style>
+ #modal {
+    top: 15%;
+    left: 50%;
+    translate: -50% -15%;
+    @apply rounded-lg absolute bg-secondary w-[300px] min-h-[500px] py-5 flex items-center flex-col justify-center relative;
+  }
+</style>
