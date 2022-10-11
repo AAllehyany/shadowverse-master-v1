d2f148fc6cc2e22000303b2d21d946ffe6f42f62
Ahmad Allehyany <aallehyany@gmail.com>
2022-10-11 07:32:54 +0300

    modified colors and borders

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,17 +4,20 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"primary": "#191327",
+				"primary": "#272232",
 				"primary-light": "#353135",
 				"secondary": "#dfe3e6",
 				"secondary-dark": "#333C42",
-				"accent": "#3d89a1"
+				"accent": "#E6ECEC"
 			},
 			fontFamily: {
 				sans: ['Roboto', 'sans-serif'],
 				heading: ['Fira Sans', 'sans-serif'],
 				'oswald': ['Oswald', 'sans-serif'],
 				'merriweather': ['Merriweather', 'sans-serif']
+			},
+			opacity: {
+				'8': '.08'
 			}
 		},
 	},
